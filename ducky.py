a64620f599e0a25156eda731c64014e0f0ad9cf6
munucrafts <sarthak@takeleap.in>
2024-06-08 18:31:51 +0530

    init

---
+++ ducky.py
@@ -0,0 +1,69 @@
+import pygame
+import math
+import random
+
+class ducky():
+    def __init__(self, window, image, position):
+        self.window = window
+        self.image = image
+        self.angle = 0
+        self.speed = 1
+        self.current_sprite = 0
+        self. particle_num = 0
+        self.flip = False
+        self.position = pygame.Vector2(position)
+        self.target_position = pygame.Vector2(500, 500)  
+        window.blit(image, position)
+
+    def move_to_next_location(self, move):
+        if move:
+            direction = self.target_position - self.position
+            self.angle = math.degrees(math.atan2(direction.y, direction.x))
+
+            radians = math.radians(self.angle)
+            horizontal = math.cos(radians) * self.speed
+            vertical = math.sin(radians) * self.speed
+            distance_to_target = direction.length()
+
+            if distance_to_target > self.speed:
+                self.position.x += horizontal
+                self.position.y += vertical
+            else:
+                self.position = self.target_position
+                self.target_position = self.get_random_position(move)
+
+            if horizontal < 0:
+                new_image = pygame.transform.flip(self.image, True, False)
+                self.flip = True
+            elif horizontal > 0:
+                new_image = pygame.transform.flip(self.image, False, False)
+                self.flip = False
+
+            self.window.blit(new_image, self.position)
+
+        else:
+            self.position = self.get_random_position(move)
+            new_image = pygame.transform.flip(self.image, self.flip, False)
+            self.window.blit(new_image, self.position)
+
+    def get_random_position(self, move):
+        if move:
+            random_position = pygame.Vector2(
+                random.randint(0, self.window.get_width() - self.image.get_width()),
+                random.randint(0, self.window.get_height() - self.image.get_height())
+            )
+            return random_position
+        else:
+            mouse_position = pygame.mouse.get_pos()
+            return pygame.Vector2(mouse_position) - (50, 50)
+    
+    def play_animation(self, sprite_array, anim_speed):
+        self.current_sprite += anim_speed
+        if self.current_sprite >= len(sprite_array):
+            self.current_sprite = 0
+        self.image = sprite_array[int(self.current_sprite)]
+
+    def play_quack_sound(self, sound):
+        if not pygame.mixer.get_busy() :
+            if sound:
+                sound.play()
---
+++ main.py
@@ -0,0 +1,89 @@
+import pygame
+import math
+import ctypes
+import win32gui
+import win32con
+from ducky import ducky
+
+#init
+pygame.init()
+pygame.mixer.init()
+
+#Create Fullscreen Window
+pygame.display.set_caption("Desktop Ducky")
+WINDOW = pygame.display.set_mode((0, 0), pygame.NOFRAME)
+
+#Make window layered
+hwnd = pygame.display.get_wm_info()["window"]
+ctypes.windll.user32.SetWindowLongW(hwnd, -20, ctypes.windll.user32.GetWindowLongW(hwnd, -20) | 0x80000)
+
+# Make the entire window transparent
+transparency_color = (0, 0, 0)
+color_key = (transparency_color[2] << 16) | (transparency_color[1] << 8) | transparency_color[0]
+ctypes.windll.user32.SetLayeredWindowAttributes(hwnd, color_key, 0, 0x00000001)
+
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#Load all Ducky Idle Sprites
+DUCKY_IDLE_01 = pygame.transform.scale(pygame.image.load("Assets\Sprites\idle\idle01.png"), (100, 100))
+DUCKY_IDLE_02 = pygame.transform.scale(pygame.image.load("Assets\Sprites\idle\idle02.png"), (100, 100))
+DUCKY_IDLE_ARRAY = [DUCKY_IDLE_01, DUCKY_IDLE_02]
+
+#Load all Ducky walk Sprites
+DUCKY_WALK_01 = pygame.transform.scale(pygame.image.load("Assets\Sprites\walk\walk01.png"), (100, 100))
+DUCKY_WALK_02 = pygame.transform.scale(pygame.image.load("Assets\Sprites\walk\walk02.png"), (100, 100))
+DUCKY_WALK_03 = pygame.transform.scale(pygame.image.load("Assets\Sprites\walk\walk03.png"), (100, 100))
+DUCKY_WALK_04 = pygame.transform.scale(pygame.image.load("Assets\Sprites\walk\walk04.png"), (100, 100))
+DUCKY_WALK_05 = pygame.transform.scale(pygame.image.load("Assets\Sprites\walk\walk05.png"), (100, 100))
+DUCKY_WALK_06 = pygame.transform.scale(pygame.image.load("Assets\Sprites\walk\walk06.png"), (100, 100))
+DUCKY_WALK_ARRAY = [DUCKY_WALK_01, DUCKY_WALK_02, DUCKY_WALK_03, DUCKY_WALK_04, DUCKY_WALK_05, DUCKY_WALK_06]
+
+#Load Quack Sound
+QUACK_SOUND = pygame.mixer.Sound("Assets\Sounds\quack_sound.mp3")
+
+#Create a Ducky instance
+ducky_instantce = ducky(WINDOW, DUCKY_IDLE_01, (900, 400))
+move = True
+amim = DUCKY_WALK_ARRAY
+anim_speed = 0.15
+quack_audio = None
+
+#Game Loop with 60 FPS
+run = True
+transparent_surface = pygame.Surface(WINDOW.get_size(), pygame.SRCALPHA)
+
+while run:
+    clock.tick(FPS)
+
+    #Keep Window on Top
+    win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOSIZE)
+
+    #Draw TransparentCircle Surface Behind the Ducky
+    pygame.draw.circle(transparent_surface, (0, 0, 0), ducky_instantce.position + (50, 50), 60)
+    WINDOW.blit(transparent_surface, (0, 0))
+    
+    #Event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            move = False
+            amim = DUCKY_IDLE_ARRAY
+            anim_speed = 0.07
+            quack_audio = QUACK_SOUND
+        elif event.type == pygame.MOUSEBUTTONUP:
+            move = True
+            amim = DUCKY_WALK_ARRAY
+            anim_speed = 0.15
+            quack_audio = None
+
+    #Move to Random Locations and set animations
+    ducky_instantce.move_to_next_location(move)
+    ducky_instantce.play_animation(amim, anim_speed)
+    ducky_instantce.play_quack_sound(quack_audio)
+
+    pygame.display.update()
+
+pygame.quit()
